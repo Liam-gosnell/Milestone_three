d4b58245373d7f4e6ab8c03341cb60d6094e1f25
Liam-gosnell <liamgoz99@gmail.com>
2020-05-16 07:52:46 +0000

    initial commit

--- app.py
+++ app.py
@@ -1,15 +1,24 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, session, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+
+
+
 
 app = Flask(__name__)
 
+app.config["MONGO_DBNAME"] = 'cinematic_base'
+app.config["MONGO_URI"] = 'mongodb+srv://liam:liam_r00t@myfirstcluster-eu72b.mongodb.net/cinematic_base?retryWrites=true&w=majority'
 
-@app.route('/')
-def hello():
-    return 'Hello World ...again'
+mongo = PyMongo(app)
+
+
+
 
 
 if __name__ == '__main__':
+    app.secret_key = 'mysecret'
     app.run(host=os.environ.get('IP'),
             port=int(os.environ.get('PORT')),
             debug=True)
