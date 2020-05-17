a144dda3c4b401ef4f87e4241da34de7a5d8446f
Liam-gosnell <liamgoz99@gmail.com>
2020-05-17 19:33:16 +0000

    added addmovie.html and all its content , viewmovie.html, added functionality in app.py

--- app.py
+++ app.py
@@ -14,6 +14,28 @@ app.config["MONGO_URI"] = 'mongodb+srv://liam:liam_r00t@myfirstcluster-eu72b.mon
 mongo = PyMongo(app)
 
 
+@app.route('/')
+def index():
+    return render_template('index.html', 
+                           movies=mongo.db.movies.find())
+
+@app.route('/add_movie')
+def add_movie():
+    return render_template('addmovie.html',
+                           categories=mongo.db.categories.find())
+
+
+@app.route('/insert_movie', methods=['POST'])
+def insert_movie():
+    movies =  mongo.db.movies
+    movies.insert_one(request.form.to_dict())
+    return redirect(url_for('index'))
+
+@app.route('/view_movie')
+def view_movie():
+    return render_template('viewmovie.html',
+                           movies=mongo.db.movies.find())
+
 
 
 
