e7e32e9fa742162668b813d03011f52cd42a9ec5
Liam-gosnell <liamgoz99@gmail.com>
2020-05-22 11:05:17 +0000

    addition of login.html and profile.html and its python code

--- app.py
+++ app.py
@@ -1,10 +1,27 @@
 import os
-from flask import Flask, render_template, redirect, session, request, url_for
+from flask import Flask, render_template, redirect, session, request, url_for, g, session, abort
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
+from random import randint
 
+class User:
+    def __init__(self, id, username, password):
+        self.id = id
+        self.username = username
+        self.password = password
+
+    def __repr__(self):
+        return f'<User: {self.username}>'
+
+users = []
+users.append(User(id=1, username='Anthony', password='password'))
+users.append(User(id=2, username='Becca', password='secret'))
+users.append(User(id=3, username='Carlos', password='somethingsimple'))
 
 app = Flask(__name__)
+app.secret_key = 'somesecretkeythatonlyiknow'
+
+
 
 app.config["MONGO_DBNAME"] = 'cinematic_base'
 app.config["MONGO_URI"] = 'mongodb+srv://liam:liam_r00t@myfirstcluster-eu72b.mongodb.net/cinematic_base?retryWrites=true&w=majority'
@@ -15,6 +32,40 @@ mongo = PyMongo(app)
 @app.route('/')
 def home():
     return render_template('home.html')
+
+@app.before_request
+def before_request():
+    g.user = None
+
+    if 'user_id' in session:
+        user = [x for x in users if x.id == session['user_id']][0]
+        g.user = user
+        
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        session.pop('user_id', None)
+
+        username = request.form['username']
+        password = request.form['password']
+        
+        user = [x for x in users if x.username == username][0]
+        if user and user.password == password:
+            session['user_id'] = user.id
+            return redirect(url_for('profile'))
+
+        return redirect(url_for('login'))
+
+    return render_template('login.html')
+
+@app.route('/profile')
+def profile():
+    if not g.user:
+        return redirect(url_for('login'))
+
+    return render_template('profile.html')
+
 
 @app.route('/index')
 def index():
@@ -71,10 +122,6 @@ def delete_movie(movie_id):
     return redirect(url_for('index'))
 
 
-@app.route('/favourites')
-def favourites():
-    return render_template('favourites.html', 
-                           movies=mongo.db.movies.find())
 
 
 
