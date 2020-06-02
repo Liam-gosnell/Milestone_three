f179e5adaf4bff5f861775b2d0db0a19f2a366ad
Liam-gosnell <liamgoz99@gmail.com>
2020-06-02 22:41:53 +0000

    debug false

--- app.py
+++ app.py
@@ -172,4 +172,4 @@ if __name__ == '__main__':
     app.secret_key = 'mysecret'
     app.run(host=os.environ.get('IP', '0.0.0.0'),
             port=int(os.environ.get('PORT', '5000')),
-            debug=True)
+            debug=False)
