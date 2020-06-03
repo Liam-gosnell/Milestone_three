a8580bd3c69c418c764b1244cdbcf07021e890cd
Liam-gosnell <liamgoz99@gmail.com>
2020-06-03 09:46:49 +0000

    env.py deleted

--- env.py
+++
@@ -1,5 +0,0 @@
-import os
-
-os.environ["MONGO_URI"] = "mongodb+srv://liam:liam_r00t@myfirstcluster-eu72b.mongodb.net/cinematic_base?retryWrites=true&w=majority"
-os.environ["MONGO_DBNAME"] = "cinematic_base"
-os.environ["SECRET_KEY"] = "mysecretpasswordformilestone3"
